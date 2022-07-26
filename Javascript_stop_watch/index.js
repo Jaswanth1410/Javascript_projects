a1d068ffbed528bd08afa40424b2b444c555097b
Jaswanth1410 <jaswanthsaikakavakam@gmail.com>
2022-07-26 16:22:53 +0530

    first commit

---
+++ Javascript_stop_watch/index.js
@@ -0,0 +1,10 @@
+var milli_sec = 0;
+var sec = 0;
+var min = 0;
+var hours = 0;
+
+var present_time = document.getElementsByName
+
+document.querySelector(".start_timer").addEventListener(onclick , () =>{
+
+});
